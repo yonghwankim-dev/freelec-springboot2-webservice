6eb6560376843180ad56325bceede82ed865ce91
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-04-29 16:32:47 +0900

    fix: 배포 스크립트 수정

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,8 @@ git pull
 
 echo "> 프로젝트 Build 시작"
 
+chmod +x ./gradlew
+
 ./gradlew build
 
 echo "> 디렉토리로 이동"
@@ -28,11 +30,11 @@ CURRENT_PID=$(pgrep -f ${PROJECT_NAME}.*.jar)
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID"]; then
-        echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+  echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-        echo "> kill -15 $CURRENT_PID"
-        kill -15 $CURRENT_PID
-        sleep 5
+  echo "> kill -15 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
 fi
 
 echo "> 새 애플리케이션 배포"
