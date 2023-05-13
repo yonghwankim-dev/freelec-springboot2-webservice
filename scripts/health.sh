18baf3d5328784c8a1f3e37192eac747b831d0f2
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-05-13 20:01:01 +0900

    docs: RESPONSE, UP_COUNT 에코 추가

--- scripts/health.sh
+++ scripts/health.sh
@@ -18,6 +18,9 @@ for RETRY_COUNT in {1..10}; do
   # RESPONSE에서 'real' 문자열을 검색하여 몇번 나타나는지를 셉니다.
   # wc -l : 검색된 결과에서 라인 수를 세어 반환합니다.
 
+  echo "> RESPONSE : $RESPONSE"
+  echo "> UP_COUNT : $UP_COUNT"
+
   if [ ${UP_COUNT} -ge 1 ]; then # $up_count >= 1 ("real" 문자열이 있는지 검증)
     echo "> Health check 성공"
     switch_proxy
